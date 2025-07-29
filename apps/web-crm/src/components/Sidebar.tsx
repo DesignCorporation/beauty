1f8273126b789f9b84d05b44b6b595c06bc252c3
DesignCorporation <info@designcorp.eu>
2025-07-29 21:14:47 +0200

    fix: Remove fixed positioning from sidebar to fix content overlap

--- apps/web-crm/src/components/Sidebar.tsx
+++ apps/web-crm/src/components/Sidebar.tsx
@@ -31,7 +31,7 @@ export default function Sidebar() {
   const location = useLocation();
 
   return (
-    <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
+    <div className="sidebar-container">
       <div className="flex-1 flex flex-col min-h-0 bg-white border-r border-gray-200">
         {/* Logo and salon info */}
         <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
